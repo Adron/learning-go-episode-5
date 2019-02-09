b4677734ecece2e758a05cbdb84c0714270f6839
Adron Hall <adron@thrashingcode.com>
2019-02-09 12:01:27 -0800

    Adding anonymous function examples and other code.

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ func main() {
 	value, err := Lookup(keySearchingFor)
 	check(err)
 	fmt.Println(value)
+
+	fmt.Printf("The square of 4 is %d.\n", square(4))
+	fmt.Printf("The negative of 4 is %d.\n", negative(4))
+	fmt.Printf("The product of 4 and 4 is %d.\n", product(4, 4))
+	fmt.Printf("The addition of 4 and 4 is %d.\n", add(4, 4))
+	fmt.Printf("The division of 8 and 4 is %d.\n", divide(8, 4))
+
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
 }
 
 func check(err error) {
