bed90388c880ddbe8f466868b07a48aed3f1b510
Adron Hall <adron@thrashingcode.com>
2019-02-09 11:47:29 -0800

    A basic error handler.

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var keySearchingFor string
+	keySearchingFor = "Cat"
+	value, err := Lookup(keySearchingFor)
+	check(err)
+	fmt.Println(value)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Lookup(key string) (string, error) {
+	if key == "Dog" {
+		return "", fmt.Errorf("Key %s isn't here.", key)
+	}
+	if key == "Cat" {
+		return "Sally", nil
+	}
+	return "wat", nil
 }
