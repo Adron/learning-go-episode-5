99893a9809c9ae22f9871e87efd809e7f7fd5cf5
Adron Hall <adron@thrashingcode.com>
2019-02-09 12:10:57 -0800

    Panic example.

--- main.go
+++ main.go
@@ -3,42 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var keySearchingFor string
-	keySearchingFor = "Cat"
-	value, err := Lookup(keySearchingFor)
-	check(err)
-	fmt.Println(value)
 
-	fmt.Printf("The square of 4 is %d.\n", square(4))
-	fmt.Printf("The negative of 4 is %d.\n", negative(4))
-	fmt.Printf("The product of 4 and 4 is %d.\n", product(4, 4))
-	fmt.Printf("The addition of 4 and 4 is %d.\n", add(4, 4))
-	fmt.Printf("The division of 8 and 4 is %d.\n", divide(8, 4))
+	choose := false
 
-	f := squares()
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-}
-
-func check(err error) {
-	if err != nil {
-		fmt.Println(err)
+	if choose {
+		fmt.Println("All is well.")
+	} else {
+		panic(fmt.Sprintln("Oh my gerd! Ugh this!"))
 	}
-}
 
-func Lookup(key string) (string, error) {
-	if key == "Dog" {
-		return "", fmt.Errorf("Key %s isn't here.", key)
-	}
-	if key == "Cat" {
-		return "Sally", nil
-	}
-	return "wat", nil
 }
