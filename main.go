20a7b3b22d8ef603bd5caafdcc5dddbd28435605
Adron Hall <adron@thrashingcode.com>
2019-02-09 10:30:53 -0800

    First function look.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	var this, result int
+	var that, message string
+	this = 2
+	that = "42"
+
+	result, message = exampleExecutor(this, that)
+
+	fmt.Printf("%s\n%d", message, result)
+}
+
+func exampleExecutor(this int, that string) (int, string) {
+
+	fmt.Printf("Numbers: %d, %s\n", this, that)
+
+	return this, "This is the result: " + that
 }
